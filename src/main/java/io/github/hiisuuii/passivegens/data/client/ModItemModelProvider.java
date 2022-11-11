1c37e5ad3d40909a79c853e4b966db9410156e87
hiisuuii <dogglez7777@gmail.com>
2022-11-11 15:43:45 -0700

    Added items

--- src/main/java/io/github/hiisuuii/passivegens/data/client/ModItemModelProvider.java
+++ src/main/java/io/github/hiisuuii/passivegens/data/client/ModItemModelProvider.java
@@ -2,6 +2,7 @@ package io.github.hiisuuii.passivegens.data.client;
 
 import io.github.hiisuuii.passivegens.core.PassiveGens;
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -15,10 +16,19 @@ public class ModItemModelProvider extends ItemModelProvider{
 	@Override
 	protected void registerModels() {
 		withExistingParent("diamond_generator", mcLoc("block/diamond_block"));
+
+		builder(itemGenerated,"generator_core");
+		builder(itemGenerated,"component_tier1");
+		builder(itemGenerated,"component_tier2");
+		builder(itemGenerated,"component_tier3");
+		builder(itemGenerated,"component_tier4");
 	}
 	
 	ModelFile itemGenerated = getExistingFile(mcLoc("item/generated"));
 	
-	getBuilder("dummy_item").parent(itemGenerated).texture("layer0","item/dummy_item");
+	
+	private ItemModelBuilder builder(ModelFile itemGenerated, String name) {
+		return getBuilder(name).parent(itemGenerated).texture("layer0", "item/"+name);
+	}
 	
 }
--- src/main/java/io/github/hiisuuii/passivegens/init/ItemInit.java
+++ src/main/java/io/github/hiisuuii/passivegens/init/ItemInit.java
@@ -1,14 +1,23 @@
 package io.github.hiisuuii.passivegens.init;
 
 import io.github.hiisuuii.passivegens.core.PassiveGens;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PassiveGens.MOD_ID);
-    
+
+    public static final RegistryObject<Item> GENERATOR_CORE = ITEMS.register("generator_core", 
+    		() -> new Item(new Item.Properties()) );
+    public static final RegistryObject<Item> COMPONENT_TIER1 = ITEMS.register("component_tier1", 
+    		() -> new Item(new Item.Properties()) );
+    public static final RegistryObject<Item> COMPONENT_TIER2 = ITEMS.register("component_tier2", 
+    		() -> new Item(new Item.Properties()) );
+    public static final RegistryObject<Item> COMPONENT_TIER3 = ITEMS.register("component_tier3", 
+    		() -> new Item(new Item.Properties()) );
+    public static final RegistryObject<Item> COMPONENT_TIER4 = ITEMS.register("component_tier4", 
+    		() -> new Item(new Item.Properties()) );
+
 }
